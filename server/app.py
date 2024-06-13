bf6cb544f9433de97a8fd038dd55d302173f6b2f
Fiona Mellink <fimellink@gmail.com>
2024-06-13 16:26:50 +1000

    Completes code along

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -9,6 +7,7 @@ from models import db, User, Review, Game
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#json prints on several lines with indents
 app.json.compact = False
 
 migrate = Migrate(app, db)
@@ -18,6 +17,44 @@ db.init_app(app)
 @app.route('/')
 def index():
     return "Index for Game/Review/User API"
+
+@app.route('/games')
+def games():
+
+    games = []
+    for game in Game.query.all():
+        game_dict = {
+            "title": game.title,
+            #skippping id as title is unique
+            "genre": game.genre,
+            "platform": game.platform,
+            "price": game.price,
+        }
+        games.append(game_dict)
+
+    response = make_response(
+        #jsonify serializes its arguments as JSON and returns a Response object
+        jsonify(games),
+        200,
+        #by default is HTML
+        {"Content-Type": "application/json"}
+    )
+
+    return response
+
+@app.route('/games/<int:id>')
+def game_by_id(id):
+    game = Game.query.filter(Game.id==id).first()
+
+    game_dict = game.to_dict()
+        
+    response = make_response(
+        jsonify(game_dict),
+        200,
+    )
+    response.headers["Content-Type"] = "application/json"
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
